06a326f25a45be1a0bec222b249446c344a4597e
Vadym <vadymcdevelweb@gmail.com>
2024-01-19 16:13:31 +0200

    add cart icon

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import s from './index.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
 
 function Header() {
   return (
     <div className={s.root}>
       <p className={s.header}>Double Pizza</p>
-      <div></div>
+      <div>
+        <FontAwesomeIcon className={s.cart} icon={faCartPlus} />
+      </div>
     </div>
   )
 }
