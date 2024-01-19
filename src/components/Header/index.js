ec05a02a42a9e2160dccd1a535b20bf65de54232
Vadym <vadymcdevelweb@gmail.com>
2024-01-19 16:33:13 +0200

    add a primitiva modal

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import s from './index.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCartPlus } from '@fortawesome/free-solid-svg-icons'
+import Modal from '../Modal'
 
 function Header() {
+  const [visible, setVisible] = useState(false)
+  const modelText = () => {
+    return (
+      <div>List of the Pizza</div>
+    )
+  }
   return (
-    <div className={s.root}>
-      <p className={s.header}>Double Pizza</p>
-      <div>
-        <FontAwesomeIcon className={s.cart} icon={faCartPlus} />
+    <>
+      <Modal
+        visible={visible}
+        setVisible={setVisible}
+        modalTitle={'Cart'}
+        modalText={modelText}
+      />
+      <div className={s.root}>
+        <p className={s.header}>Double Pizza</p>
+        <div onClick={() => setVisible(true)}>
+          <FontAwesomeIcon className={s.cart} icon={faCartPlus} />
+        </div>
       </div>
-    </div>
+    </>
   )
 }
 
